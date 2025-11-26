dbbe05a006bb74059dec905f3e01a849d914b3c2
Bimal <bimal.devasia@geekywolf.com>
2025-11-26 19:30:47 +0530

    solved

--- RSVP.Application/Interfaces/Interface1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using RSVP.Domain.Entities;
-
-namespace RSVP.Application.Interfaces;
-
-public interface  IEventAccessService
-{
-    Task<bool> IsOrganizerOrOwnerAsync(int eventId, CancellationToken ct);
-}
