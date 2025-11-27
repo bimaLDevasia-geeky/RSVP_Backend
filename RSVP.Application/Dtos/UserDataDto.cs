7ff2269aa7dd12073ff9e7d2c87fcd8af91ba81e
arjun-geekywolf <237754090+arjun-geekywolf@users.noreply.github.com>
2025-11-27 11:19:31 +0530

    refactor:folder name changed usercommandvalidatoradded,

--- RSVP.Application/Dtos/UserDataDto.cs
+++ RSVP.Application/Dtos/UserDataDto.cs
@@ -21,25 +21,7 @@ public class UserDataDto
 }
 
 
-public class EventDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
-    public DateTime Date { get; set; }
-    public string Venue { get; set; } = null!;
-    public TimeOnly Time { get; set; }
-    public bool IsPublic { get; set; }
-    public EventStatus Status { get; set; }
-    public AttendiesRole? MyRole { get; set; }
-    public AttendiesStatus? MyResponseStatus { get; set; }
-}
 
-public class NotificationDto
-{
-    public int Id { get; set; }
-    public string Description { get; set; } = null!;
-    public DateTime CreatedAt { get; set; }
-    public string Route { get; set; } = null!;
-}
+
+
 
