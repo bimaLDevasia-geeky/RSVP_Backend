ba2869d98f0ceffd3ddb2dd611f6a54b5be7c5d3
arjun-geekywolf <237754090+arjun-geekywolf@users.noreply.github.com>
2025-11-27 12:25:33 +0530

    feat: implement notification feature with query and handler

---
+++ RSVP.API/Controllers/NotificationController.cs
@@ -0,0 +1,26 @@
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RSVP.Application.Features.Notification.Queries;
+
+namespace RSVP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public NotificationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetNotifications()
+        {
+            GetNotificationQuery query = new GetNotificationQuery();
+            return Ok(await _mediator.Send(query));
+        }
+    }
+}
--- RSVP.Application/Features/Notification/Commands/CreateNotification/CreateNotificationCommandHandler.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace RSVP.Application.Features.Notification.Command.CreateNotification;
-
-public class CreateNotificationCommandHandler
-{
-
-}
