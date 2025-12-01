f61f7e16abdf8004ad074db91ead7d7c101acd63
arjun-geekywolf <237754090+arjun-geekywolf@users.noreply.github.com>
2025-12-01 14:14:28 +0530

    Add attendieId to EventDto and update mapping in GetUserByIdQueryHandler

--- RSVP.Application/Dtos/EventDto.cs
+++ RSVP.Application/Dtos/EventDto.cs
@@ -12,6 +12,7 @@ public class EventDto
     public string Venue { get; set; } = null!;
     public TimeOnly Time { get; set; }
     public bool IsPublic { get; set; }
+    public int? attendieId { get; set; }
     public EventStatus Status { get; set; }
     public AttendiesRole? MyRole { get; set; }
     public AttendiesStatus? MyResponseStatus { get; set; }
